1637f7016c7757601e8ac4d63c3cac6d43ef1945
Rostyslav Brovchenko <rostyslavbrv@gmail.com>
2023-05-05 15:17:26 +0300

    Added models

---
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
+
+        public ICollection<PokemonOwner> PokemonOwner { get; set; }
+
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
+    }
+}
